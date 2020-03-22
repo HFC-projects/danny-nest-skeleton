327ebed8893c8440753126f387342c28eae2cc64
Danny Idlis <danny.idlis@gmail.com>
2020-03-22 21:54:32 +0200

    Added basic swagger

--- src/components/teachers/teachers.controller.ts
+++ src/components/teachers/teachers.controller.ts
@@ -1,19 +1,26 @@
 import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { TeachersService } from './teachers.service';
 import { Teacher } from './interfaces/teacher.interface';
+import * as HttpStatusCodes from 'http-status-codes';
 
+@ApiTags('Teachers')
 @Controller('Teachers')
 export class TeachersController {
 	constructor(private readonly teacherService: TeachersService) { }
 
 	@Get()
+	@ApiResponse({ status: HttpStatusCodes.OK, description: 'The query was fulfilled successfully'})
+	@ApiResponse({ status: HttpStatusCodes.INTERNAL_SERVER_ERROR, description: 'An error occured'})
 	getAllTeachers(): Promise<Teacher[]> {
 		return this.teacherService.getAll();
 	}
 
 	@Get(':id')
-	async getTeacherById(@Param('id') id): Promise<Teacher> {
+	@ApiResponse({ status: HttpStatusCodes.OK, description: 'The query was fulfilled successfully'})
+	@ApiResponse({ status: HttpStatusCodes.NOT_FOUND, description: 'The given ID was not found'})
+	@ApiResponse({ status: HttpStatusCodes.INTERNAL_SERVER_ERROR, description: 'An error occured'})
+	async getTeacherById(@Param('id') id: string): Promise<Teacher> {
 		return this.teacherService.getById(id);
 	}
-
 }
