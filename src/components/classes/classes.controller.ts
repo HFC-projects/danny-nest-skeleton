07e2d0520a849f268bff955100c0dc8a353f3098
Danny Idlis <danny.idlis@gmail.com>
2020-03-23 11:44:01 +0200

    Added some swagger comments

--- src/components/classes/classes.controller.ts
+++ src/components/classes/classes.controller.ts
@@ -1,17 +1,25 @@
 import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { ClassesService } from './classes.service';
 import { Class } from './interfaces/class.interface';
+import * as HttpStatusCodes from 'http-status-codes';
 
+@ApiTags('Classes')
 @Controller('Classes')
 export class ClassesController {
 	constructor(private readonly classesService: ClassesService) { }
 
 	@Get()
+	@ApiResponse({ status: HttpStatusCodes.OK, description: 'The query was fulfilled successfully'})
+	@ApiResponse({ status: HttpStatusCodes.INTERNAL_SERVER_ERROR, description: 'An error occured'})
 	getAllClasses(): Promise<Class[]> {
 		return this.classesService.getAll();
 	}
 
 	@Get(':id')
+	@ApiResponse({ status: HttpStatusCodes.OK, description: 'The query was fulfilled successfully'})
+	@ApiResponse({ status: HttpStatusCodes.NOT_FOUND, description: 'The given ID was not found'})
+	@ApiResponse({ status: HttpStatusCodes.INTERNAL_SERVER_ERROR, description: 'An error occured'})
 	async getClassById(@Param('id') id): Promise<Class> {
 		return this.classesService.getById(id);
 	}
--- src/components/students/students.controller.ts
+++ src/components/students/students.controller.ts
@@ -1,17 +1,25 @@
 import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { StudentsService } from './students.service';
 import { Student } from './interfaces/student.interface';
+import * as HttpStatusCodes from 'http-status-codes';
 
+@ApiTags('Students')
 @Controller('Students')
 export class StudentsController {
 	constructor(private readonly studentsService: StudentsService) { }
 
 	@Get()
+	@ApiResponse({ status: HttpStatusCodes.OK, description: 'The query was fulfilled successfully'})
+	@ApiResponse({ status: HttpStatusCodes.INTERNAL_SERVER_ERROR, description: 'An error occured'})
 	getAllStudents(): Promise<Student[]> {
 		return this.studentsService.getAll();
 	}
 
 	@Get(':id')
+	@ApiResponse({ status: HttpStatusCodes.OK, description: 'The query was fulfilled successfully'})
+	@ApiResponse({ status: HttpStatusCodes.NOT_FOUND, description: 'The given ID was not found'})
+	@ApiResponse({ status: HttpStatusCodes.INTERNAL_SERVER_ERROR, description: 'An error occured'})
 	async getStudentById(@Param('id') id): Promise<Student> {
 		return this.studentsService.getById(id);
 	}
